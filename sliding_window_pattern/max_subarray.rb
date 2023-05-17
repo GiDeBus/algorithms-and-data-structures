6e9ac9511b7bfa0c9cc7b9392e4f858e60ec4883
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-05-16 22:26:44 -0400

    change name of variables

--- sliding_window_pattern/max_subarray.rb
+++ sliding_window_pattern/max_subarray.rb
@@ -1,13 +1,14 @@
 
 
 def max_sub_array(nums)
-  max = nums[0]
+  sum = nums[0]
   temp_sum = 0
 
   nums.each do |element|
       temp_sum += element
-      max = temp_sum if temp_sum > max
+      sum = temp_sum if temp_sum > sum
       temp_sum = 0 if temp_sum < 0
   end
-  max
+
+  sum
 end
